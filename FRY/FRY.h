7f36170945da9039e0c13929fe7c053c9b989328
Brian King <brian.king@raizlabs.com>
2014-10-03 22:40:44 -0400

    Initial Commit

---
+++ FRY/FRY.h
@@ -0,0 +1,13 @@
+//
+//  FRY.h
+//  FRY
+//
+//  Created by Brian King on 10/3/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRY : NSObject
+
+@end
