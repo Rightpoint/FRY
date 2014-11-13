d673f7c2de4c7a4ba1dd92338a76b06e99bf8ad5
Brian King <brian.king@raizlabs.com>
2014-11-13 14:48:11 -0500

    Copy in the enable accessibility code from KIF

--- FRY/FRYDefines.m
+++ FRY/FRYDefines.m
@@ -8,6 +8,45 @@
 
 #import "FRYDefines.h"
 #import <CommonCrypto/CommonCrypto.h>
+#import <dlfcn.h>
+#import <objc/runtime.h>
 
 NSTimeInterval const kFRYEventDispatchInterval = 0.01;
 NSString *kFRYCheckFailedExcetion = @"FRY Failed Check";
+
+@interface FRYCleanupSimulator : NSObject
+@end
+
+@implementation FRYCleanupSimulator
++ (void)load
+{
+    @autoreleasepool {
+        [self enableAccessibility];
+    }
+}
+
++ (void)enableAccessibility;
+{
+    NSString *appSupportLocation =  @"/System/Library/PrivateFrameworks/AppSupport.framework/AppSupport";
+    
+    NSDictionary *environment = [[NSProcessInfo processInfo] environment];
+    NSString *simulatorRoot = [environment objectForKey:@"IPHONE_SIMULATOR_ROOT"];
+    if (simulatorRoot) {
+        appSupportLocation = [simulatorRoot stringByAppendingString:appSupportLocation];
+    }
+    
+    void *appSupportLibrary = dlopen([appSupportLocation fileSystemRepresentation], RTLD_LAZY);
+    
+    CFStringRef (*copySharedResourcesPreferencesDomainForDomain)(CFStringRef domain) = dlsym(appSupportLibrary, "CPCopySharedResourcesPreferencesDomainForDomain");
+    
+    if (copySharedResourcesPreferencesDomainForDomain) {
+        CFStringRef accessibilityDomain = copySharedResourcesPreferencesDomainForDomain(CFSTR("com.apple.Accessibility"));
+        
+        if (accessibilityDomain) {
+            CFPreferencesSetValue(CFSTR("ApplicationAccessibilityEnabled"), kCFBooleanTrue, accessibilityDomain, kCFPreferencesAnyUser, kCFPreferencesAnyHost);
+            CFRelease(accessibilityDomain);
+        }
+    }
+}
+
+@end
