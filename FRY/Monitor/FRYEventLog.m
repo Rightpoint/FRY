b6e1561416ede8acc08225505e1325962d42ebe2
Brian King <brian.king@raizlabs.com>
2014-12-22 22:50:44 -0500

    Basic pathing

--- FRY/Monitor/FRYEventLog.m
+++ FRY/Monitor/FRYEventLog.m
@@ -10,4 +10,58 @@
 
 @implementation FRYEventLog
 
++ (NSURL *)eventLogDirectory
+{
+    NSURL *eventLogDirectory = [[[NSFileManager defaultManager] URLsForDirectory:NSUserDirectory inDomains:NSUserDomainMask] lastObject];
+    return eventLogDirectory;
+}
+
++ (NSArray *)loadFromDirectory:(NSURL *)directory
+{
+    return @[];
+}
+
++ (NSDateFormatter *)dateFormatter
+{
+    static NSDateFormatter *formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"yyyy_MMM_EEE_HH:mm:ss"];
+    });
+    return formatter;
+}
+
+- (NSString *)eventLogNameForPath
+{
+    return [[self.name componentsSeparatedByString:@" "] componentsJoinedByString:@"_"];
+}
+
+- (NSString *)eventLogDateForPath
+{
+    return [self.class.dateFormatter stringFromDate:self.startingDate];
+}
+
+- (NSURL *)eventBundlePath
+{
+    NSString *combined = [@[[self eventLogDateForPath], [self eventLogNameForPath]] componentsJoinedByString:@"-_-"];
+    return [[self.class eventLogDirectory] URLByAppendingPathComponent:[combined stringByAppendingString:@".fry"]];
+}
+
+- (NSURL *)eventLogPath
+{
+    return [[self eventBundlePath] URLByAppendingPathComponent:@"EventLog.plist"];
+}
+
+- (BOOL)save:(NSError **)error
+{
+    return YES;
+}
+
+- (BOOL)load:(NSError **)error
+{
+    return YES;
+}
+
+
 @end
