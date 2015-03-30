b87924439bd46c1ed79f7170119e0632a891c8f7
Brian King <brian.king@raizlabs.com>
2015-03-29 20:52:14 -0400

    Typing tweak

--- FRY/Typing/FRYTypist.m
+++ FRY/Typing/FRYTypist.m
@@ -7,11 +7,34 @@
 //
 
 #import "FRYTypist.h"
-#import "UIKit+FRYCastCompanions.h"
 #import "NSObject+FRYLookup.h"
 #import "FRYTouchDispatch.h"
 #import "FRYTouch.h"
 #import "UIKit+FRYExposePrivate.h"
+
+
+/**
+ * It's a pain to bring "type safety" to private classes.  These are the private classes, with the FRY prefix instead of UI.
+ * These classes never actually exist, but we cast to them to get some help from the compiler.
+ */
+@interface FRYKBKeyTree : NSObject
+
+- (BOOL)isLetters;
+- (BOOL)isShiftKeyplane;
+- (id)keys;
+- (id)keyplaneForKey:(id)arg1;
+- (struct CGRect)frame;
+
+@end
+
+@interface FRYKeyboardLayoutStar : UIView
+
+@property(retain, nonatomic) FRYKBKeyTree *activeKey;
+@property(readonly, nonatomic) FRYKBKeyTree *keyplane;
+@property(readonly, nonatomic) FRYKBKeyTree *keyboard;
+- (FRYKBKeyTree *)baseKeyForString:(NSString *)arg1;
+
+@end
 
 @implementation FRYTypist
 
